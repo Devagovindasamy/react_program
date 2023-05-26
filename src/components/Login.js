513b5bdd267d0e1a8910889220960a84c122203b
Devagovindasamy <134516149+Devagovindasamy@users.noreply.github.com>
2023-05-26 19:24:14 +0530

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,46 @@
-import React,{useState} from 'react'
-
-function Login() {
-    const[email,setEmail]=useState("")
-    const[pass,setPass]=useState("")
-    function submitHandler(){
-        if (email ==="deva@gmail.com" & pass==="1234"){
-            alert("success")
-        }
-            else{
-                alert("failed")
-            }
-
-    }
-  return (
-    <form onSubmit={submitHandler}>
-        <div>
-            <label htmlFor='email'>Email : </label>
-            <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}> </input>
-        </div>
-        <div>
-            <label htmlFor='pass'>Password :</label>
-            <input type="text" value={pass} onChange={(e)=>setPass(e.target.value)}> </input>
-        </div>
-    </form> 
-  )
+import React,{Component} from "react";
+class Login extends Component
+{
+constructor(props)
+{
+    super(props)
+    this.state={
+        sname:'',
+        pwd:''
+    }   
+}
+nameHandler=(event)=>{
+    this.setState({
+        sname:event.target.value})
 }
 
+pwdHandler=(event)=>{
+    this.setState({
+        pwd:event.target.value})
+}
+
+submitHandler=(event)=>{
+    alert(
+        "name:"+`${this.state.sname}`+
+        "pwd:"+`${this.state.pwd}`
+        )
+    }
+render()
+{
+    return(<form onSubmit={this.submitHandler}>
+     <div>
+        <label> User name</label>
+        <input type="text" value={this.state.sname} onChange={this.nameHandler}></input>
+     </div>
+     <div>
+     <label>Password</label>
+        <input type="text" value={this.state.pwd} onChange={this.pwdHandler}></input>
+      
+     </div>
+     <div>
+        <button>submit</button>
+     </div>
+    </form>)
+}
+}
 export default Login
